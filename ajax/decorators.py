c14f734411eb336a1caab005e56c0a15900d3cca
Joe Stump <joe@joestump.net>
2011-03-02 10:15:35 -0800

    Added documentation about security and added a  decorator.

---
+++ ajax/decorators.py
@@ -0,0 +1,8 @@
+from django.utils.translation import ugettext as _
+from decorator import decorator
+from ajax import AJAXError
+
+@decorator
+def login_required(f, *args, **kwargs):
+    if not args[0].user.is_authenticated():
+        raise AJAXError(403, _('User must be authenticated.'))
