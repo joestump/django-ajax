6d802ff9f5fa8ed8b86a039c6a67bb32c5722b87
Joe Stump <joe@joestump.net>
2011-05-07 15:38:16 -0700

    Minor PEP8 fix.

--- tests/example/endpoints.py
+++ tests/example/endpoints.py
@@ -3,6 +3,7 @@ from ajax.decorators import login_required
 from ajax.endpoints import ModelEndpoint, BaseEndpoint
 from ajax.exceptions import AJAXError
 
+
 @login_required
 def echo(request):
     """For testing purposes only."""
