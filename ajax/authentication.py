e044915f84f6406a3f428f7fdb58690092d29427
Joe Stump <joe@joestump.net>
2013-08-21 10:00:12 -0700

    Revert "Merge pull request #42 from cam-stitt/master"
    This reverts commit 6dca8253824d201b9c6ea6916645aeeea612043d, reversing
    changes made to ab936f9f8dac2e5420ddd04b3724a4fbba5d002f.

--- ajax/authentication.py
+++
@@ -1,6 +0,0 @@
-class BaseAuthentication(object):
-    def is_authenticated(self, request, application, method):
-        if request.user.is_authenticated():
-            return True
-
-        return False
--- ajax/conf.py
+++
@@ -1,5 +0,0 @@
-from appconf import AppConf
-
-
-class AjaxAppConf(AppConf):
-    AJAX_AUTHENTICATION = 'ajax.authentication.BaseAuthentication'
