b0d6c4daa93ba9048c340eba0a05fe1144ef68c6
Justin Abrahms <justin@abrah.ms>
2014-03-28 12:00:03 -0700

    Minor bump for length on list envelopes.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ajax',
-    version='1.0.1',
+    version='1.1.0',
     description='A simple framework for creating AJAX endpoints in Django.',
     long_description='',
     keywords='django, ajax',
