74a2c30a3149b3eb757a456e18ecc5e000098968
Nicholas Serra <nicks@ydekproductions.com>
2014-10-16 15:36:53 -0700

    DeprecationWarning fix

--- tests/settings.py
+++ tests/settings.py
@@ -141,6 +141,7 @@ LOGGING = {
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
+            'filters': [],
             'class': 'django.utils.log.AdminEmailHandler'
         }
     },
