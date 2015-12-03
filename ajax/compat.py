74885c5740c7a277732c3326cdfd09d56600c832
Justin Mcclure <j.mcclure@pdx.edu>
2015-12-03 13:55:55 -0800

    Add getLogger to ajax.compat

--- ajax/compat.py
+++ ajax/compat.py
@@ -4,7 +4,9 @@ import django
 if django.VERSION >= (1, 7):
     from django.utils.module_loading import import_string as path_to_import
     from importlib import import_module
+    from logging import getLogger
 else:
     # 1.4 LTS compatibility
     from ajax.utils import import_by_path as path_to_import
     from django.utils.importlib import import_module
+    from django.utils.log import getLogger
